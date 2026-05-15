4a6b89d7340d59a331e6b949dd79334658e52cfc
Enzo <Enzo.schmidt@edu.unifil.br>
2026-05-14 21:53:33 -0300

    Finalizei a implementação do removedor de Nós

--- Atividade1/Main.java
+++ Atividade1/Main.java
@@ -4,8 +4,13 @@ class Main{
         arvoreBinaria.inserir(10);
         arvoreBinaria.inserir(4);
         arvoreBinaria.inserir(7);
+        arvoreBinaria.inserir(3);
+        arvoreBinaria.inserir(15);
+        arvoreBinaria.inserir(13);
+        arvoreBinaria.inserir(16);
+        arvoreBinaria.inserir(14);
 
-        arvoreBinaria.remover(10);
+        arvoreBinaria.remover(15);
         arvoreBinaria.exibir("Pos");
 
     }
